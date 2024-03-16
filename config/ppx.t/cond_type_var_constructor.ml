d248987cc1795de99d3735c06635dbd355d4d642
Kevin Fox <kevin_fox@me.com>
2024-03-16 08:46:45 -0400

    tests: add more not expression tests (#12)

--- config/ppx.t/cond_type_var_constructor.ml
+++ config/ppx.t/cond_type_var_constructor.ml
@@ -2,21 +2,31 @@ type band =
   | Rush
   | Yes
   | KingCrimson [@config (made_up = true)]
+  | TheXX [@config (not (value = "1"))]
+  | TheWho [@config (not (the_who))]
+  | Beatles [@config (not (value = true))]
 
 (* this pattern matching is exhaustive because the config removes the
-   KingCrimson constructor *)
+   KingCrimson & TheXX constructor *)
 let best_band_in_the_world x =
   match x with
   | Rush -> true
   | Yes -> false
+  | TheWho -> false
+  | Beatles -> false
 
 type band_polyvar = [
   | `Rush
   | `Yes
   | `KingCrimson [@config (made_up=true)]
+  | `TheXX [@config (not (value = "1"))]
+  | `TheWho [@config (not (the_who))]
+  | `Beatles [@config (not (value = true))]
 ]
 
 let the_best_band_in_the_world (x: band_polyvar) =
   match x with
   | `Rush -> true
   | `Yes -> false
+  | `TheWho -> false
+  | `Beatles -> false
