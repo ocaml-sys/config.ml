40f6e6c8df61523f39a40be9f81222e8cdff231d
KFoxder <kevin_fox@me.com>
2024-04-18 12:26:09 -0400

    Feature: Apply config on module structs and signatures

--- config/ppx.t/cond_module.ml
+++ config/ppx.t/cond_module.ml
@@ -1,7 +1,24 @@
-module M = struct
-  let best_band_in_the_world = "RUSH"
+module type M = sig
+  val best_band_in_the_world : string [@@cfg (value = "1")]
+  val worst_band_in_the_world : string [@@cfg (value = "2")]
+  val get_lower_case_band_name : string -> string [@@cfg (value = "2")]
+  val get_upper_case_band_name : string -> string [@@cfg (value = "1")]
 end
-[@@config (made_up = true)]
 
-let rush () = M.best_band_in_the_world
-[@@config (made_up = true)]
+module O:M = struct
+  let best_band_in_the_world = "RUSH" [@@cfg (value = "1")]
+  let worst_band_in_the_world = "Nickelback" [@@cfg (value = "2")]
+  let get_lower_case_band_name name = name [@@cfg (value = "2")]
+  let get_upper_case_band_name name = name [@@cfg (value = "1")]
+end
+
+module N : sig
+  val best_band_in_the_world : string [@@cfg (value = "1")]
+  val worst_band_in_the_world : string [@@cfg (value = "2")]
+end  =  struct
+  let best_band_in_the_world = "RUSH" [@@cfg (value = "1")]
+  let worst_band_in_the_world = "Nickelback" [@@cfg (value = "2")]
+end 
+
+let rush () = N.best_band_in_the_world
+let nickelback () = N.worst_band_in_the_world [@@cfg (value = "2")]
--- config/ppx.t/run.t
+++ config/ppx.t/run.t
@@ -148,3 +148,36 @@
       cookies = []
     }]
   external foo : unit -> int = "made_up_call"
+
+  $ dune clean
+  $ value=1 target_os=madeup dune describe pp cond_module.ml
+  [@@@ocaml.ppx.context
+    {
+      tool_name = "ppx_driver";
+      include_dirs = [];
+      load_path = [];
+      open_modules = [];
+      for_package = None;
+      debug = false;
+      use_threads = false;
+      use_vmthreads = false;
+      recursive_types = false;
+      principal = false;
+      transparent_modules = false;
+      unboxed_types = false;
+      unsafe_string = false;
+      cookies = []
+    }]
+  module type M  =
+    sig
+      val best_band_in_the_world : string[@@cfg value = "1"]
+      val get_upper_case_band_name : string -> string[@@cfg value = "1"]
+    end
+  module O : M =
+    struct
+      let best_band_in_the_world = "RUSH"[@@cfg value = "1"]
+      let get_upper_case_band_name name = name[@@cfg value = "1"]
+    end 
+  module N : sig val best_band_in_the_world : string[@@cfg value = "1"] end =
+    struct let best_band_in_the_world = "RUSH"[@@cfg value = "1"] end 
+  let rush () = N.best_band_in_the_world
